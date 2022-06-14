86c1851e24c28855cfb0537035fde5496e4dfbde
Helena Rasche <hxr@hx42.org>
2022-06-14 14:45:24 +0200

    add runner

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # toolshed-version-database
+
+I've needed since years something to map tool IDs + versions back to which repository revision I need to install. I understand the toolshed is miserable to work with, so, fair, but, I still need this so we're just going to scrape it.
+
+## LICENSE
+
+AGPL-3.0
---
+++ tmp.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# this is how to get tool info from the toolshed
+import bioblend
+from bioblend import toolshed
+import tqdm
+toolshed_name = 'toolshed.g2.bx.psu.edu'
+toolshed_url = f'https://{toolshed_name}'
+
+processed = []
+with open('tool-version-database.tsv', 'r') as handle:
+    data = [x.split('\t') for x in handle.readlines()]
+    for x in data:
+        processed.append(
+            (x[0], x[1])
+        )
+
+# Dunno if that last one is fully processed, all revisions of it.
+if len(processed) > 0:
+    last_processed = processed[-1]
+    processed = list(set(processed))
+    processed.remove(last_processed)
+
+
+ts = toolshed.ToolShedInstance(url=toolshed_url)
+
+for repo in tqdm.tqdm(ts.repositories.get_repositories()):
+    key = (repo['name'], repo['owner'])
+    if key in processed:
+        continue
+
+    for rev in ts.repositories.get_ordered_installable_revisions(repo['name'], repo['owner']):
+        try:
+            e = ts.repositories.get_repository_revision_install_info(repo['name'], repo['owner'], rev)
+        except bioblend.ConnectionError:
+            print(f"Could not fetch the results of {repo['name']} {repo['owner']} {rev}")
+
+        if 'valid_tools' not in e[1]:
+            res = [
+                repo['name'],
+                repo['owner'],
+                rev,
+                'invalid',
+                'invalid',
+                'invalid',
+            ]
+            print('\t'.join(res))
+
+            continue
+
+        for valid_tool in e[1]['valid_tools']:
+            res = [
+                repo['name'],
+                repo['owner'],
+                rev,
+                valid_tool['id'],
+                valid_tool['guid'],
+                valid_tool['version'],
+            ]
+            print('\t'.join(res))
