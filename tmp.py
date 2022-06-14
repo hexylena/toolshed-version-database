3e15bb21c9495a8d8c6bd1b59c657ae42143c611
Helena Rasche <hxr@hx42.org>
2022-06-14 15:44:05 +0200

    make it multi

--- tmp.py
+++ tmp.py
@@ -1,35 +1,18 @@
 #!/usr/bin/env python3
 import sys
-# this is how to get tool info from the toolshed
 import bioblend
 from bioblend import toolshed
-import tqdm
-toolshed_name = 'toolshed.g2.bx.psu.edu'
-toolshed_url = f'https://{toolshed_name}'
+from tqdm.contrib.concurrent import thread_map
 
-processed = []
-with open('tool-version-database.tsv', 'r') as handle:
-    data = [x.split('\t') for x in handle.readlines()]
-    for x in data:
-        processed.append(
-            (x[0], x[1])
-        )
 
-# Dunno if that last one is fully processed, all revisions of it.
-if len(processed) > 0:
-    last_processed = processed[-1]
-    processed = list(set(processed))
-    processed.remove(last_processed)
-
-from multiprocessing import Pool
-
-def fetch_versions(x):
+def fetch_versions(repo):
     ret = []
     for rev in ts.repositories.get_ordered_installable_revisions(repo['name'], repo['owner']):
         try:
             e = ts.repositories.get_repository_revision_install_info(repo['name'], repo['owner'], rev)
         except bioblend.ConnectionError:
             sys.stderr.write(f"Could not fetch the results of {repo['name']} {repo['owner']} {rev}\n")
+            continue
 
         if 'valid_tools' not in e[1]:
             res = [
@@ -40,8 +23,7 @@ def fetch_versions(x):
                 'invalid',
                 'invalid',
             ]
-            print('\t'.join(res))
-
+            ret.append(res)
             continue
 
         for valid_tool in e[1]['valid_tools']:
@@ -53,46 +35,15 @@ def fetch_versions(x):
                 valid_tool['guid'],
                 valid_tool['version'],
             ]
-            print('\t'.join(res))
+            ret.append(res)
+    return ret
 
 
 
-ts = toolshed.ToolShedInstance(url=toolshed_url)
+ts = toolshed.ToolShedInstance(url='https://toolshed.g2.bx.psu.edu')
+repos = ts.repositories.get_repositories()
 
-with Pool(4) as p:
-    print(p.map(fetch_versions, ts.repositories.get_repositories()))
-
-for repo in tqdm.tqdm(ts.repositories.get_repositories()):
-    key = (repo['name'], repo['owner'])
-    if key in processed:
-        continue
-
-    for rev in ts.repositories.get_ordered_installable_revisions(repo['name'], repo['owner']):
-        try:
-            e = ts.repositories.get_repository_revision_install_info(repo['name'], repo['owner'], rev)
-        except bioblend.ConnectionError:
-            sys.stderr.write(f"Could not fetch the results of {repo['name']} {repo['owner']} {rev}\n")
-
-        if 'valid_tools' not in e[1]:
-            res = [
-                repo['name'],
-                repo['owner'],
-                rev,
-                'invalid',
-                'invalid',
-                'invalid',
-            ]
-            print('\t'.join(res))
-
-            continue
-
-        for valid_tool in e[1]['valid_tools']:
-            res = [
-                repo['name'],
-                repo['owner'],
-                rev,
-                valid_tool['id'],
-                valid_tool['guid'],
-                valid_tool['version'],
-            ]
-            print('\t'.join(res))
+r = thread_map(fetch_versions, repos, max_workers=10)
+for repo_result in r:
+    for rev_result in repo_result:
+        print("\t".join(rev_result))
