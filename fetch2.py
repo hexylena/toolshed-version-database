5b6539800093d75b4a4962e1c700335f5e710f1f
Helena Rasche <hexylena@galaxians.org>
2024-06-27 17:22:24 +0200

    Add tool metadata fetcher

---
+++ fetch2.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+import os
+import requests
+import json
+import sys
+import time
+from tqdm.contrib.concurrent import thread_map
+
+SERVERS = [
+    "usegalaxy.eu",
+    "usegalaxy.fr",
+    "usegalaxy.org",
+    "usegalaxy.org.au",
+    # 
+    "usegalaxy.be",
+    "usegalaxy.cz",
+    "usegalaxy.no",
+    "usegalaxy.es",
+    # The rest of the public servers list.
+    "cpt.tamu.edu/galaxy-public",
+    "galaxy-web.ipk-gatersleben.de",
+    "galaxy.bio.di.uminho.pt",
+    "galaxy.hyphy.org",
+    "galaxy.mesocentre.uca.fr",
+    "galaxy.pasteur.fr",
+    "galaxytrakr.org",
+    "hyperbrowser.uio.no/coloc-stats",
+    "iris.angers.inra.fr/galaxypub-cfbp",
+    "mississippi.sorbonne-universite.fr",
+    "neo.engr.uconn.edu",
+    "palfinder.ls.manchester.ac.uk",
+    "vm-chemflow-francegrille.eu",
+    "www.immportgalaxy.org",
+]
+
+headers = {
+    'User-Agent': 'hexylena-bot@1 (https://github.com/hexylena/toolshed-version-database/)',
+}
+
+def seq_try(path, servers):
+    for server in servers:
+        try:
+            return requests.get(f"https://{server}{path}", timeout=10, headers=headers).json()
+        except Exception as e:
+            print(e)
+            pass
+
+
+def fetch_name(tool_id):
+    meta = seq_try(f"/api/tools/{tool_id}?io_details=True&link_details=False", SERVERS)
+    if meta is None or "name" not in meta:
+        return None
+    else:
+        return [
+            tool_id,
+            meta["name"],
+            meta["description"],
+            ",".join(meta["edam_operations"]),
+            ",".join(meta["edam_topics"]),
+        ]
+
+
+with open("guid-rev.json") as f:
+    guid_rev = json.load(f)
+    tool_ids = set(guid_rev.keys())
+print(f"Found {len(tool_ids)} tools")
+
+if os.path.exists("tool_meta.tsv"):
+    with open("tool_meta.tsv") as f:
+        existing = set([line.split("\t")[0] for line in f])
+        tool_ids = tool_ids - existing
+print(f"Fetching {len(tool_ids)} tools")
+
+r = thread_map(fetch_name, list(tool_ids), max_workers=10)
+with open("tool_meta.tsv", "a") as f:
+    results = [x for x in list(r) if x is not None]
+    print(f"Fetched {len(results)} updates")
+    for repo_result in results:
+        f.write("\t".join(repo_result) + "\n")
