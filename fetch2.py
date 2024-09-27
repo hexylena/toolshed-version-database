f733d31985fb6812d96e74af58d43012518b8220
Helena Rasche <hexylena@galaxians.org>
2024-09-27 11:51:52 +0200

    load in some local tools

--- fetch2.py
+++ fetch2.py
@@ -1,37 +1,11 @@
 #!/usr/bin/env python3
+import glob
 import os
 import requests
 import json
 import sys
 import time
 from tqdm.contrib.concurrent import thread_map
-
-SERVERS = [
-    "usegalaxy.eu",
-    "usegalaxy.fr",
-    "usegalaxy.org",
-    "usegalaxy.org.au",
-    #
-    "usegalaxy.be",
-    "usegalaxy.cz",
-    "usegalaxy.no",
-    "usegalaxy.es",
-    # The rest of the public servers list.
-    "cpt.tamu.edu/galaxy-public",
-    "galaxy-web.ipk-gatersleben.de",
-    "galaxy.bio.di.uminho.pt",
-    "galaxy.hyphy.org",
-    "galaxy.mesocentre.uca.fr",
-    "galaxy.pasteur.fr",
-    "galaxytrakr.org",
-    "hyperbrowser.uio.no/coloc-stats",
-    "iris.angers.inra.fr/galaxypub-cfbp",
-    "mississippi.sorbonne-universite.fr",
-    "neo.engr.uconn.edu",
-    "palfinder.ls.manchester.ac.uk",
-    "vm-chemflow-francegrille.eu",
-    "www.immportgalaxy.org",
-]
 
 
 def fetch_name(tool_id):
@@ -59,37 +33,28 @@ def fetch_name(tool_id):
 
 
 
-with open("guid-rev.json") as f:
-    guid_rev = json.load(f)
-    tool_ids = set(guid_rev.keys())
-print(f"Found {len(tool_ids)} tools")
+tool_ids = glob.glob("api/**/*", recursive=True)
+tool_ids = [x for x in tool_ids if os.path.isfile(x)]
+tool_ids = [x[len("api/tools/"):] for x in tool_ids]
 
-if os.path.exists("tool-meta.tsv"):
-    with open("tool-meta.tsv") as f:
-        existing = set([line.split("\t")[0] for line in f])
-        tool_ids = tool_ids - existing
-print(f"{len(tool_ids)} tools are missing metadata")
 
 r = thread_map(fetch_name, list(tool_ids), max_workers=10)
-with open("tool-meta.tsv", "a") as f:
+tool_meta = {}
+
+with open("tool-meta.tsv", "w") as f:
     results = [x for x in list(r) if x is not None]
     print(f"Fetched {len(results)} updates")
     for repo_result in results:
         f.write("\t".join(repo_result) + "\n")
 
-with open("tool-meta.tsv", "r") as handle:
-    data = [x.strip("\n").split("\t") for x in handle]
-
-tool_meta = {}
-
-for guid, name, description, edam_operations, edam_topics, bio_tools in data:
-    tool_meta[guid] = {
-        "name": name,
-        "desc": description,
-        "edam_operations": edam_operations.split(","),
-        "edam_topics": edam_topics.split(","),
-        "bio.tools": bio_tools,
-    }
+        (guid, name, description, edam_operations, edam_topics, bio_tools) = repo_result
+        tool_meta[guid] = {
+            "name": name,
+            "desc": description,
+            "edam_operations": edam_operations.split(","),
+            "edam_topics": edam_topics.split(","),
+            "bio.tools": bio_tools,
+        }
 
 with open("tool-meta.json", "w") as handle:
     json.dump(tool_meta, handle)
