974ba27798d5991c3e9ef34f53fe734fae90137b
Helena Rasche <hexylena@galaxians.org>
2024-09-30 09:41:15 +0200

    some more cleanups

--- fetch2a.py
+++ fetch2a.py
@@ -17,7 +17,6 @@ SERVERS = [
     "usegalaxy.no",
     "usegalaxy.es",
     # The rest of the public servers list.
-    "cpt.tamu.edu/galaxy-public",
     "galaxy-web.ipk-gatersleben.de",
     "galaxy.bio.di.uminho.pt",
     "galaxy.hyphy.org",
@@ -39,17 +38,24 @@ headers = {
 
 
 def seq_try(path, servers):
+    # print(f"seq_try {path}")
     for server in servers:
+        # print(f"  → {server}")
         try:
-            return requests.get(
+            d = requests.get(
                 f"https://{server}{path}", timeout=10, headers=headers
             ).json()
-        except Exception as e:
-            print(e)
+            if 'err_msg' in d:
+                continue
+            # print(f"  success")
+            return d
+        except Exception:
+            # print(f"Failed to fetch {server}{path}")
             pass
 
 
 def download_api(tool_id):
+    # print(f"Downloading {tool_id}")
     if tool_id.count("/") > 4:
         tool_id_without_version = "/".join(tool_id.split("/")[0:-1])
     else:
@@ -57,14 +63,17 @@ def download_api(tool_id):
 
     # If exactly this tool id was already downloaded, skip
     if os.path.exists("api/tools/" + tool_id):
+        # print("Already downloaded")
         return None
 
     meta = seq_try(f"/api/tools/{tool_id}?io_details=True&link_details=False", SERVERS)
     if meta is None or "name" not in meta:
+        # print("Meta is none:", meta)
         return None
     else:
         # make dir
         os.makedirs("api/tools/" + tool_id_without_version, exist_ok=True)
+        print(f"Downloaded {tool_id}")
         with open("api/tools/" + tool_id, "w") as handle:
             json.dump(meta, handle)
 
@@ -74,4 +83,13 @@ with open("guid-rev.json") as f:
     tool_ids = set(guid_rev.keys())
 print(f"Found {len(tool_ids)} tools")
 
-thread_map(download_api, list(tool_ids), max_workers=10)
+
+# Existing 
+import glob
+existing_tool_ids = glob.glob("api/**/*", recursive=True)
+existing_tool_ids = [x for x in existing_tool_ids if os.path.isfile(x)]
+existing_tool_ids = set([x[len("api/tools/"):] for x in existing_tool_ids])
+
+tool_ids = list(tool_ids - existing_tool_ids)
+print(f"Reduced to {len(tool_ids)} tools")
+thread_map(download_api, tool_ids, max_workers=1)
